d82e464d2cdc040fbd421e149ac1b5ba710a6b62
DaniRomeroo <daniromero212@gmail.com>
2021-10-20 17:56:33 +0200

    Parte 3 - Ejercicio 1

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -8,42 +8,40 @@ import { NgForm } from '@angular/forms';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [personaje1, personaje2, personaje3, personaje4, personaje5];
+  personaje1: Personaje = {
+    nombre: 'Goku',
+    poder: 100
+  }
+  personaje2: Personaje = {
+    nombre: 'Vegeta',
+    poder: 100
+  }
+  personaje3: Personaje = {
+    nombre: 'Freezer',
+    poder: 95
+  }
+  personaje4: Personaje = {
+    nombre: 'Trunks',
+    poder: 85
+  }
+  personaje5: Personaje = {
+    nombre: 'Krilin',
+    poder: 90
+  }
+
+  personajes: Personaje[] = [this.personaje1, this.personaje2, this.personaje3, this.personaje4, this.personaje5];
 
   registrarPersonaje(form: NgForm) {
     
     let nuevoPersonaje: Personaje = {
       nombre: form.value.nombre,
       poder: form.value.poder
-    };
-
+    }
     this.personajes.push(nuevoPersonaje);
-
   }
 }
 
 interface Personaje {
   nombre: string,
   poder: number,
-};
-
-let personaje1: Personaje = {
-  nombre: 'Goku',
-  poder: 100
-};
-let personaje2: Personaje = {
-  nombre: 'Vegeta',
-  poder: 100
-};
-let personaje3: Personaje = {
-  nombre: 'Freezer',
-  poder: 95
-};
-let personaje4: Personaje = {
-  nombre: 'Trunks',
-  poder: 85
-};
-let personaje5: Personaje = {
-  nombre: 'Krilin',
-  poder: 90
 };
